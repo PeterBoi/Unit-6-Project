dc657d9926ecce5322cf446cb603d720fea107b1
Pedro Cruz Avalos <106279254+PeterBoi@users.noreply.github.com>
2022-06-19 17:10:10 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-$(".start-button").click(function() {
-
-
+$(".water-falls").hide();
+$(".taco-stand").hide();
+$(".jumping-waterFalls").hide();
+$(".eating-tacos").hide();
+$(".choice-Micos").hide();
+$(".choice-Tacos").hide();
+$(".final_waterFalls").hide();
+$(".final_tacoStand").hide();
+$(".ending").hide();
+// when the water fall option is chosen 
+$(".waterFall-button").click(function() {
+    $(".choice").hide();
+    $(".intro").hide();
+    $(".tacoStand-button").hide();
+    $(".waterFall-button").hide();
+    $(".water-falls").show();
+    $(".choice-Micos").show();
+    $(".jumping-waterFalls").show();
+});
+// to end the water fall journey
+$(".jumping-waterFalls").dblclick(function() {
+    $(".water-falls").hide();
+    $(".choice-Micos").hide();
+    $(".jumping-waterFalls").hide();
+    $(".ending").show();
+    $(".final_waterFalls").show();
+});
+// when the taco stand option is chosen
+$(".tacoStand-button").click(function() {
+    $(".choice").hide();
+    $(".intro").hide();
+    $(".tacoStand-button").hide();
+    $(".waterFall-button").hide();
+    $(".taco-stand").show();
+    $(".choice-Tacos").show();
+    $(".eating-tacos").show();
+});
+// to end the taco stand journey
+$(".eating-tacos").dblclick(function() {
+    $(".taco-stand").hide();
+    $(".choice-Tacos").hide();
+    $(".eating-tacos").hide();
+    $(".ending").show();
+    $(".final_tacoStand").show();
 });
